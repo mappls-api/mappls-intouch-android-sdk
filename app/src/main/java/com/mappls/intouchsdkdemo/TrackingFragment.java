a10db6df2997b083fd9d38f64239a841c12195dd
katariadeepak86 <kataria.deepak86@gmail.com>
2023-08-25 12:58:33 +0530

    Update TrackingFragment.java

--- app/src/main/java/com/mappls/intouchsdkdemo/TrackingFragment.java
+++ app/src/main/java/com/mappls/intouchsdkdemo/TrackingFragment.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,8 +15,6 @@ import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import com.mappls.intouchsdkdemo.databinding.FragmentTrackingBinding;
-import com.mappls.sdk.bleplugin.BleScanResultCallback;
-import com.mappls.sdk.bleplugin.BluetoothLEScanHelper;
 import com.mappls.sdk.intouch.InTouch;
 import com.mappls.sdk.tracking.Config;
 import com.mappls.sdk.tracking.LocationChangeObserver;
@@ -27,7 +24,7 @@ import com.mappls.sdk.tracking.utils.TrackingError;
 
 public class TrackingFragment extends Fragment implements TrackingStateObserver.OnTrackingStateChangeListener, LocationChangeObserver.OnLocationStateChangeListener {
     private FragmentTrackingBinding mBinding;
-    private BleScanResultCallback mBleScanResultCallback = null;
+    //   private BleScanResultCallback mBleScanResultCallback = null;
     // Enter user device Mac Address
     private String bleMacAddress = "";
 
@@ -41,8 +38,8 @@ public class TrackingFragment extends Fragment implements TrackingStateObserver.
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_tracking, container, false);
-        String bleAddress = BluetoothLEScanHelper.getInstance().getBleDeviceMacId();
-        mBinding.textMacId.setText(bleAddress != null ? bleAddress : bleMacAddress);
+        //   String bleAddress = BluetoothLEScanHelper.getInstance().getBleDeviceMacId();
+        //  mBinding.textMacId.setText(bleAddress != null ? bleAddress : bleMacAddress);
         mBinding.setIsBeaconEnabled(InTouch.isRunning());
         InTouch.addNotificationIconsAndTitle(R.drawable.ic_stat_phone_iphone,
                 R.drawable.ic_stat_phone_iphone,
@@ -55,22 +52,22 @@ public class TrackingFragment extends Fragment implements TrackingStateObserver.
             if (getActivity() == null) {
                 return;
             }
-            if (TextUtils.isEmpty(mBinding.textMacId.getText().toString())) {
+           /* if (TextUtils.isEmpty(mBinding.textMacId.getText().toString())) {
                 Toast.makeText(requireActivity(), "Please enter Ble device mac address", Toast.LENGTH_SHORT).show();
-            } else {
-                startTracking();
-            }
+            } else {*/
+            startTracking();
+            //}
         });
         mBinding.setOnClickRedirect(v -> {
             startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://intouch.mapmyindia.com")));
         });
         InTouch.addTrackingStateListener(this);
-        if (mBleScanResultCallback == null) {
+       /* if (mBleScanResultCallback == null) {
             mBleScanResultCallback = externalSensor -> {
                 mBinding.txtDeviceCode.setText("\nTemperature:- " + externalSensor.getTemperature() + "\nHumidity:- " + externalSensor.getHumidity());
             };
             BluetoothLEScanHelper.getInstance().addBleScanResultListener(mBleScanResultCallback);
-        }
+        }*/
         return mBinding.getRoot();
     }
 
@@ -92,7 +89,7 @@ public class TrackingFragment extends Fragment implements TrackingStateObserver.
             new Config.Builder(getContext())
                     .setPriority(InTouch.BEACON_PRIORITY_FAST)
                     .build();
-            BluetoothLEScanHelper.getInstance().setBleDeviceMacId(mBinding.textMacId.getText().toString());
+            //   BluetoothLEScanHelper.getInstance().setBleDeviceMacId(mBinding.textMacId.getText().toString());
             InTouch.startTracking();
         } else {
             stopTracking();
@@ -125,8 +122,8 @@ public class TrackingFragment extends Fragment implements TrackingStateObserver.
         super.onDestroyView();
         mBinding = null;
         InTouch.removeTrackingStateListener(this);
-        BluetoothLEScanHelper.getInstance().removeBleScanResultListener(mBleScanResultCallback);
-        mBleScanResultCallback = null;
+       /* BluetoothLEScanHelper.getInstance().removeBleScanResultListener(mBleScanResultCallback);
+        mBleScanResultCallback = null;*/
 
     }
 
